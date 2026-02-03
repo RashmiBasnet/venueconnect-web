6315ff7b2255bcf47511450da0a521ed415fe9c6
RashmiBasnet <rashmibasnet2019@gmail.com>
2026-02-03 23:43:13 +0545

    Added Endpoints for Admin User

--- lib/api/endpoints.ts
+++ lib/api/endpoints.ts
@@ -2,5 +2,13 @@ export const API = {
     AUTH: {
         REGISTER: "/api/auth/register",
         LOGIN: "/api/auth/login"
+    },
+    ADMIN: {
+        USER: {
+            GET_ALL: "/api/admin/users",
+            GET_BY_ID: (id: string) => `/api/admin/users/${id}`,
+            UPDATE: (id: string) => `/api/admin/users/${id}`,
+            DELETE: (id: string) => `/api/admin/users/${id}`,
+        }
     }
 }
