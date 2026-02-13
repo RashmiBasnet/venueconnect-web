652d0e335d597b11ab898b2464d67d4270f9ae0b
RashmiBasnet <rashmibasnet2019@gmail.com>
2026-02-13 12:52:12 +0545

    Updated Schema and Login & Register Form Design

--- app/(auth)/schema.ts
+++ app/(auth)/schema.ts
@@ -20,3 +20,18 @@ export const registerSchema = z.object({
 );
 
 export type RegisterType = z.infer<typeof registerSchema>;
+
+export const resetPasswordSchema = z.object({
+    password: z.string().min(6, "Password should be atleast 6 characters"),
+    confirmPassword: z.string().min(6, "Confirm password should be atleast 6 characters"),
+}).refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+});
+
+export type ResetPasswordType = z.infer<typeof resetPasswordSchema>;
+
+export const requestPasswordResetSchema = z.object({
+    email: z.email()
+});
+
+export type RequestPasswordResetType = z.infer<typeof requestPasswordResetSchema>;
